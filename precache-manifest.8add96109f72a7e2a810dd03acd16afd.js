9916c6b5e4d4cf7a76294436e266df54df8c71c8
Matei Mircea <mrc_matei@yahoo.com>
2020-08-07 19:52:14 +0300

    Updates

---
+++ precache-manifest.8add96109f72a7e2a810dd03acd16afd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff5302cc97111f51013c5b7f7f2f908",
+    "url": "/cats/index.html"
+  },
+  {
+    "revision": "3ee784ca7cdaad331a4a",
+    "url": "/cats/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "244dcb83246d85f18a96",
+    "url": "/cats/static/css/main.3afc381c.chunk.css"
+  },
+  {
+    "revision": "3ee784ca7cdaad331a4a",
+    "url": "/cats/static/js/2.0450200d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cats/static/js/2.0450200d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "244dcb83246d85f18a96",
+    "url": "/cats/static/js/main.5433135a.chunk.js"
+  },
+  {
+    "revision": "fc6aa50808fdb6147e32",
+    "url": "/cats/static/js/runtime-main.be4feccd.js"
+  }
+]);
